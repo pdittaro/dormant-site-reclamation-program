65003171a804aa44e40fb23dff8f92c58dd5700c
Luke Mitchell <lukegamitchell@gmail.com>
2020-05-07 14:24:21 -0700

    Cleanup reducers.js

--- services/dsrp-web/src/reducers.js
+++ services/dsrp-web/src/reducers.js
@@ -1,34 +1,9 @@
-// Reducers
-import complianceReducerObject from "./reducers/complianceReducer";
 import authenticationReducerObject from "./reducers/authenticationReducer";
-import incidentReducerObject from "./reducers/incidentReducer";
-import mineReducerObject from "./reducers/mineReducer";
-import minespaceReducerObject from "./reducers/minespaceReducer";
 import modalReducerObject from "./reducers/modalReducer";
-import noticeOfWorkReducerObject from "./reducers/noticeOfWorkReducer";
-import partiesReducerObject from "./reducers/partiesReducer";
-import permitReducerObject from "./reducers/permitReducer";
-import reportReducerObject from "./reducers/reportReducer";
-import searchReducerObject from "./reducers/searchReducer";
 import staticContentReducerObject from "./reducers/staticContentReducer";
-import userReducerObject from "./reducers/userReducer";
-import varianceReducerObject from "./reducers/varianceReducer";
-import securitiesReducerObject from "./reducers/securitiesReducer";
 import orgbookReducerObject from "./reducers/orgbookReducer";
 
-export const complianceReducer = complianceReducerObject;
 export const authenticationReducer = authenticationReducerObject;
-export const incidentReducer = incidentReducerObject;
-export const mineReducer = mineReducerObject;
-export const minespaceReducer = minespaceReducerObject;
 export const modalReducer = modalReducerObject;
-export const noticeOfWorkReducer = noticeOfWorkReducerObject;
-export const partiesReducer = partiesReducerObject;
-export const permitReducer = permitReducerObject;
-export const reportReducer = reportReducerObject;
-export const searchReducer = searchReducerObject;
 export const staticContentReducer = staticContentReducerObject;
-export const userReducer = userReducerObject;
-export const varianceReducer = varianceReducerObject;
-export const securitiesReducer = securitiesReducerObject;
 export const orgbookReducer = orgbookReducerObject;
